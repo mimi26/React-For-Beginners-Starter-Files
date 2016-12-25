3ee7975cf600a4933054171b032b0db615a73625
mimi klein <lifeistoogoodtobetrue@gmail.com>
2016-12-25 14:35:19 -0500

    video 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     this.state = {
       fishes: {},
       order: {}
@@ -19,6 +21,12 @@ class App extends React.Component {
     fishes[`fish-${timestamp}`] = fish;
     this.setState({ fishes });
   }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
   render() {
     return (
 <div className="catch-of-the-day">
@@ -26,8 +34,7 @@ class App extends React.Component {
 <Header tagline="Fresh Seafood Market"/>
 </div>
   <Order />
-  <Inventory addFish=
-  {this.addFish} />
+  <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 </div>
       )
   }
