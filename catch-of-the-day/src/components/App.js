1ab25027fd929dbf62fc013803b864c585da6492
mimi klein <lifeistoogoodtobetrue@gmail.com>
2016-12-25 20:05:14 -0500

    video 19

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -25,11 +25,23 @@ componentWillMount() {
     context: this,
     state: 'fishes'
   });
+  const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+  if(localStorageRef) {
+    this.setState({
+      order: JSON.parse(localStorageRef)
+    });
+  }
 
 }
 
 componentWillUnmount() {
   base.removeBinding(this.ref);
+}
+
+componentWillUpdate(nextProps, nextState) {
+  localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
+
 }
 
   addFish(fish) {
@@ -63,7 +75,11 @@ componentWillUnmount() {
   }
 </ul>
 </div>
-  <Order fishes={this.state.fishes} order={this.state.order} />
+  <Order
+  fishes={this.state.fishes}
+  order={this.state.order}
+  params={this.props.params}
+  />
   <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 </div>
       )
