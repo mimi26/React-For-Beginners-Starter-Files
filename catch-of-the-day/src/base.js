23256b45566951145c7db1678427e40c1d7898ff
mimi klein <lifeistoogoodtobetrue@gmail.com>
2016-12-25 19:37:40 -0500

    video 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDLBN9vTPuhosszN4hmuhcc44H_UAWK5Ls",
+    authDomain: "catch-of-the-day-mimi-k.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-mimi-k.firebaseio.com",
+
+});
+
+export default base;
